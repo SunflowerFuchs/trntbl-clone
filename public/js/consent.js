8a56b4d27ae520ec3d77b888ad06b19cdc2dc6d4
Pascal Pirschel <pirschel@lamapoll.de>
2019-03-15 13:07:43 +0100

    Updated cookie consent
    Fixes #5

--- public/js/consent.js
+++ public/js/consent.js
@@ -1,67 +1,52 @@
-$(document).ready(function() {
-    var consentIsSet = "unknown";
-    var cookieBanner = "#cookieBanner";
-    var cookieBannerClose = "#cookieBannerClose";
-    var consentString = "cookieConsent=";
+var Consent = {
+    cookie: "cookieConsent",
 
-    // Sets a cookie granting/denying consent, and displays some text on console/banner
-    function setCookie(console_log, banner_text, consent) {
-        console.log(console_log);
-        $(cookieBanner).text(banner_text);
-        $(cookieBanner).fadeOut(2500);
-        var d = new Date();
-        var exdays = 30*12; //  1 year
-        d.setTime(d.getTime()+(exdays*24*60*60*1000));
-        var expires = "expires="+d.toGMTString();
-        document.cookie = consentString + consent + "; " + expires + ";path=/";
-        consentIsSet = consent;
-    }
-
-    function denyConsent() {
-        // disabled deny cookie; if the user denies, we don't want ANY cookies set
-        // setCookie("Consent denied", "You disallowed the use of cookies.", "false");
-        // Unbind consent-granting actions
-        $(window).unbind("scroll");
-        $("a:not(.noconsent)").unbind("click");
-    }
-
-    function grantConsent() {
-        if (consentIsSet == "true") return; // Don't grant twice
-        setCookie("Consent granted", "Thank you for accepting cookies.", "true");
-        doConsent();
-    }
-
-    $(cookieBannerClose).click(function() {
-        grantConsent();
-    });
-
-    // Run the consent code. We may be called either from grantConsent() or
-    // from the main routine
-    function doConsent() {
-        console.log("Consent was granted");
-    }
-
-    // main routine
-    //
-    // First, check if cookie is present
-    var cookies = document.cookie.split(";");
-    for (var i = 0; i < cookies.length; i++) {
-        var c = cookies[i].trim();
-        if (c.indexOf(consentString) == 0) {
-            consentIsSet = c.substring(consentString.length, c.length);
+    // Sets a cookie granting/denying consent
+    setConsentCookie: function(consent) {
+        if (consent === false) {
+            deleteAllCookies();
         }
-    }
 
-    if (consentIsSet == "unknown") {
-        $(cookieBanner).fadeIn();
-        // The two cases where consent is granted: scrolling the window or clicking a link
-        // Don't set cookies on the "cookies page" on scroll
-        var pageName = location.pathname.substr(location.pathname.lastIndexOf("/") + 1);
-        if (pageName != "my-site/cookies") $(window).scroll(grantConsent); // XXX you may edit this name
-        $("a:not(.noconsent)").click(grantConsent);
-        $(".denyConsent").click(denyConsent);
-        // allow re-enabling cookies
-        $(".allowConsent").click(grantConsent);
-    }
-    else if (consentIsSet == "true") doConsent();
-});
+        var d = new Date();
+        var exdays = 30*12; // 1 year
+        if (consent === true) { // if consent was explicitly given, just delete the cookie again
+            exdays = -1;
+        }
+
+        // map to string for clean saving
+        consent = consent === true ? "true" : "false";
+        setCookie(Consent.cookie, consent, exdays);
+    },
+
+    hasConsent: function() {
+        return getCookie(Consent.cookie) !== "false";
+    },
+
+    denyConsent: function() {
+        Consent.setConsentCookie(false);
+    },
+
+    grantConsent: function() {
+        Consent.setConsentCookie(true);
+    },
+
+    changeCookieConsent: function() {
+        Consent.setConsentCookie(!Consent.hasConsent());
+    },
+
+    initButtons: function() {
+        Consent.updateConsentButton();
+        $(".changeCookieConsent").click(function() {
+            Consent.changeCookieConsent();
+            Consent.updateConsentButton();
+        });
+    },
+
+    updateConsentButton: function() {
+        if (Consent.hasConsent()) {
+            $(".changeCookieConsent").text('Revoke cookie consent');
+        } else {
+            $(".changeCookieConsent").text('Give cookie consent');
+        }
+    },
+};
--- resources/views/trntbl/cookies.blade.php
+++ resources/views/trntbl/cookies.blade.php
@@ -11,20 +11,26 @@
     <div class="inner cover cookies">
         <h1 class="cover-heading">Cookie Policy for {{ env('APP_NAME') }}</h1>
         <h2>What Are Cookies</h2>
-        As is common practice with almost all professional websites this site uses cookies, which are tiny files that are downloaded to your computer, to improve your experience. This page describes what information they gather, how we use it and why we sometimes need to store these cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or 'break' certain elements of the sites functionality.<br />
+        Many websites store cookies on your computer.<br />
+        Cookies are tiny files that are downloaded to your computer and managed by your browser. Their purpose is to store snippets of information between visits (e.g. shopping cart contents or visual settings).<br />
+        This page describes why and for what we use cookies. We also give you the option to opt-out of our use of cookies.<br />
         <br />
         For more general information on cookies see the <a href="http://en.wikipedia.org/wiki/HTTP_cookie">Wikipedia article on HTTP Cookies...</a><br />
-        <h2>How We Use Cookies</h2>
-        We use cookies for a variety of reasons detailed below. Unfortunately is most cases there are no industry standard options for disabling cookies without completely disabling the functionality and features they add to this site. It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they are used to provide a service that you use.<br />
-        <h2>Disabling Cookies</h2>
-        You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for how to do this). Be aware that disabling cookies will affect the functionality of this and many other websites that you visit. Disabling cookies will usually result in also disabling certain functionality and features of the this site. Therefore it is recommended that you do not disable cookies.<br />
         <h2>The Cookies We Set</h2>
-        In order to provide you with a great experience on this site we provide the functionality to set your preferences for how this site runs when you use it (e.g. setting volume levels and shuffle function). In order to remember your preferences we need to set cookies so that this information can be called whenever you interact with a page is affected by your preferences.<br />
+        For some of the settings you can change (e.g. setting volume levels and shuffle function) we store the value in cookies, so that on your next visit everything is as you left it.<br />
+        <h2>Disabling Cookies</h2>
+        You can change your cookie consent by clicking this button: <button class="btn btn-sm btn-default changeCookieConsent">Revoke cookie consent</button>
         <h2>More Information</h2>
-        Hopefully that has clarified things for you and as was previously mentioned if there is something that you aren't sure whether you need or not it's usually safer to leave cookies enabled in case it does interact with one of the features you use on our site. However if you are still looking for more information then you can contact us through one of our preferred contact methods.<br />
+        Hopefully that has clarified things for you.<br />
+        Usually, unless you know what you're doing, you should leave cookies enabled, though in this case the impact of disabling cookies on this site is negligible.<br />
+        If you have any more questions, feel free to contact me.<br />
         <br />
         <strong>Email:</strong> <a href="mailto:egoisticalgoat@gmail.com">egoisticalgoat@gmail.com</a> <br />
         <strong>My blog:</strong> <a href="http://egoisticalgoat.tumblr.com">egoisticalgoat.tumblr.com</a><br />
         <br />
     </div>
+@endsection
+
+@section('scripts')
+    <script>Consent.initButtons()</script>
 @endsection
